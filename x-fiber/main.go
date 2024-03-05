e19a48b227bae9ac85162592aa484bc938956888
Gustavo Rabanal <15724234+gurame@users.noreply.github.com>
2024-03-04 21:09:35 -0500

    first revision of go fiber

---
+++ x-fiber/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"log"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(ctx *fiber.Ctx) error {
+		return ctx.SendString("Hello, World!")
+	})
+
+	log.Fatal(app.Listen(":3000"))
+}
